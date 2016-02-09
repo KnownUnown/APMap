8b3bf066a0edf155d63aff562526c7d38225188f
KnownUnown <andrew.tianyi.pan@gmail.com>
2016-02-08 20:50:31 -0600

    Fixed.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var map_revision = "r6";
-var map = L.map("map").setView([50, 50], 1);
+var map = L.map("map").setView([0, 0], 3);
 
 L.tileLayer("https://knownunown.github.io/APMap/assets/{revision}/map_full/{z}/{x}/{y}.png", {
   revision: map_revision,
