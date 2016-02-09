a48df7bc5d1e76c88e1d0575078f97e33ae0215e
KnownUnown <andrew.tianyi.pan@gmail.com>
2016-02-08 19:12:25 -0600

    Fixed? >.>

--- js/main.js
+++ js/main.js
@@ -4,4 +4,7 @@ var map = L.map("map").setView([50, 50], 3);
 L.tileLayer("https://knownunown.github.io/APMap/assets/{revision}/map_full/{z}/{x}/{y}.png", {
   revision: map_revision,
   attribution: "Map data courtesy of <a href=\"http://minecraftairshippirates.enjin.com/profile/1310042\">Miss Fortune</a>",
+  minZoom: 1,
+  maxZoom: 6,
+  tms: true
 }).addTo(map);
