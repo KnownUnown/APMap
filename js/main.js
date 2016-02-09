6aa17ddbd2743db98d53f06440f5ba46271851d5
KnownUnown <andrew.tianyi.pan@gmail.com>
2016-02-08 18:50:46 -0600

    Working commit?

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var map_revision = "r6";
+var map = L.map("map").setView([50, 50], 3);
+
+L.tileLayer("https://knownunown.github.io/APMap/assets/{revision}/{z}/{x}/{y}.png", {
+  revision: map_revision,
+  attribution: "Map data courtesy of <a href=\"http://minecraftairshippirates.enjin.com/profile/1310042\">Miss Fortune</a>",
+  maxZoom: 6,
+}).addTo(map);
