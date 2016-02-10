464c2adbb702380b82cfe6c7d98495f1893cc459
KnownUnown <andrew.tianyi.pan@gmail.com>
2016-02-09 21:29:41 -0600

    Settlement data added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
+var map = null;
 var map_revision = "r6";
-var map = L.map("map").setView([0, 0], 3);
 
-L.tileLayer("https://knownunown.github.io/APMap/assets/{revision}/map_full/{z}/{x}/{y}.png", {
-  revision: map_revision,
-  attribution: "Map data courtesy of <a href=\"http://minecraftairshippirates.enjin.com/profile/1310042\">Miss Fortune</a>",
-  minZoom: 1,
-  maxZoom: 6,
-  tms: true,
-  
-}).addTo(map);
+window.onload = function(){
+  map = L.map("map").setView([0, 0], 3);
+
+  L.tileLayer("assets/{revision}/map_full/{z}/{x}/{y}.png", {
+    revision: map_revision,
+    attribution: "Map data courtesy of <a href=\"http://minecraftairshippirates.enjin.com/profile/1310042\">Miss Fortune</a>",
+    minZoom: 1,
+    maxZoom: 6,
+    tms: true,
+    continuousWorld: true,
+    crs: L.CRS.Simple
+  }).addTo(map);
+}
